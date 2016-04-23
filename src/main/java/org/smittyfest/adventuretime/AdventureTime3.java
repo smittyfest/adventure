4f2284745911653d0026401aad9e8e23275b5471
Eric Smith <esmith@thebrighttag.com>
2016-04-23 14:52:57 -0500

    updated AdventureTime3

--- src/main/java/org/smittyfest/adventuretime/AdventureTime3.java
+++ src/main/java/org/smittyfest/adventuretime/AdventureTime3.java
@@ -7,64 +7,50 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import static org.smittyfest.adventuretime.Context.HORIZONTAL_MOVE_INCREMENT;
+import static org.smittyfest.adventuretime.Context.LEFTMOST_FRAME_BARRIER;
+import static org.smittyfest.adventuretime.Context.OVERWORLD_MAP_COLUMNS;
+import static org.smittyfest.adventuretime.Context.OVERWORLD_MAP_ROWS;
+import static org.smittyfest.adventuretime.Context.TOP_OF_FRAME_BARRIER;
+import static org.smittyfest.adventuretime.Context.VERTICAL_MOVE_INCREMENT;
 
 /**
  * @author smittyfest
  */
 public class AdventureTime3 extends Application {
-  private static final int HORIZONTAL_MOVE_INCREMENT = 8;
-  private static final int VERTICAL_MOVE_INCREMENT = 12;
-
-  private static final int TOP_OF_FRAME_BARRIER = 12;
-  private static final int BOTTOM_OF_FRAME_BARRIER = 641;
-
-  private static final int LEFTMOST_FRAME_BARRIER = 2;
-  private static final int RIGHTMOST_FRAME_BARRIER = 630;
-
-  private static final int OVERWORLD_MAP_WIDTH = 1001;
-  private static final int OVERWORLD_MAP_HEIGHT = 443;
-
-  private static final int OVERWORLD_MAP_ROWS_ADJUSTMENT = 12;
-  private static final int OVERWORLD_MAP_COLUMNS_ADJUSTMENT = 24;
-
-  private static final int OVERWORLD_MAP_ROWS = (OVERWORLD_MAP_HEIGHT / VERTICAL_MOVE_INCREMENT) - OVERWORLD_MAP_ROWS_ADJUSTMENT;
-  private static final int OVERWORLD_MAP_COLUMNS = (OVERWORLD_MAP_WIDTH / HORIZONTAL_MOVE_INCREMENT) - OVERWORLD_MAP_COLUMNS_ADJUSTMENT;
-
-  private static final String FONTSTYLE = "Courier";
-  private static final int FONTSIZE = 14;
-  private static final int FONT_PLAIN = 0;
-  private static final int MAX_COLOR_VALUE = 16777215;
+  private final Context ctx = new Context();
 
   private static final Random random = new Random();
 
   public void start(Stage primaryStage) throws Exception {
     GridPane board = new GridPane();
+    double pcX = random.nextInt(OVERWORLD_MAP_COLUMNS);
+    double pcY = random.nextInt(OVERWORLD_MAP_ROWS);
+    System.out.println(pcX + ", " + pcY);
+    Tile pcTile = Tile.from("-", pcX, pcY);
     for (int colCoord = LEFTMOST_FRAME_BARRIER, col = 0; col < OVERWORLD_MAP_COLUMNS; ++col, colCoord += HORIZONTAL_MOVE_INCREMENT) {
       for (int rowCoord = TOP_OF_FRAME_BARRIER, row = 0; row < OVERWORLD_MAP_ROWS; ++row, rowCoord += VERTICAL_MOVE_INCREMENT) {
         StackPane stackPane = new StackPane();
-        Tile tile = Tile.from("j", Color.BLACK, colCoord, rowCoord); // generateColor() will randomly set the tile background color
-        Text text = new Text(tile.getSymbol());
-        text.setFill(generateColor());
-        text.setFont(Font.font("Courier", FontWeight.BOLD, 16));
-        stackPane.getChildren().addAll(tile, text);
+        Tile tile = Tile.from("Q", Color.BLACK, colCoord, rowCoord); // generateColor() will randomly set the tile background color
+        if (pcX == col && pcY == row) {
+          System.out.println("Heelllllooooo!!!!" + pcX + ", " + pcY);
+          stackPane.getChildren().addAll(tile, tile.getSymbol(), pcTile, pcTile.getSymbol());
+        } else {
+          stackPane.getChildren().addAll(tile, tile.getSymbol());
+        }
         board.add(stackPane, colCoord, rowCoord);
       }
     }
     Scene scene = new Scene(board, 1010, 384);
     primaryStage.setScene(scene);
+    primaryStage.setTitle("Overworld Map");
     primaryStage.setResizable(false);
     primaryStage.show();
   }
 
   public static void main(String[] args) {
     Application.launch(args);
-  }
-
-  private Color generateColor() {
-    return Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
   }
 }
