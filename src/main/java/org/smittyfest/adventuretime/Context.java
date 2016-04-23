40cbcbdce7a4d1c71cd980e60c0f0670a0d4abc5
Eric Smith <esmith@thebrighttag.com>
2016-04-23 14:50:21 -0500

    moved constants to Context

--- src/main/java/org/smittyfest/adventuretime/Context.java
+++ src/main/java/org/smittyfest/adventuretime/Context.java
@@ -5,4 +5,21 @@ package org.smittyfest.adventuretime;
  * @since 4/21/16
  */
 public class Context {
+  public static final int HORIZONTAL_MOVE_INCREMENT = 8;
+  public static final int VERTICAL_MOVE_INCREMENT = 12;
+
+  public static final int TOP_OF_FRAME_BARRIER = 12;
+  public static final int BOTTOM_OF_FRAME_BARRIER = 641;
+
+  public static final int LEFTMOST_FRAME_BARRIER = 2;
+  public static final int RIGHTMOST_FRAME_BARRIER = 630;
+
+  public static final int OVERWORLD_MAP_WIDTH = 1001;
+  public static final int OVERWORLD_MAP_HEIGHT = 443;
+
+  public static final int OVERWORLD_MAP_ROWS_ADJUSTMENT = 12;
+  public static final int OVERWORLD_MAP_COLUMNS_ADJUSTMENT = 24;
+
+  public static final int OVERWORLD_MAP_ROWS = (OVERWORLD_MAP_HEIGHT / VERTICAL_MOVE_INCREMENT) - OVERWORLD_MAP_ROWS_ADJUSTMENT;
+  public static final int OVERWORLD_MAP_COLUMNS = (OVERWORLD_MAP_WIDTH / HORIZONTAL_MOVE_INCREMENT) - OVERWORLD_MAP_COLUMNS_ADJUSTMENT;
 }
